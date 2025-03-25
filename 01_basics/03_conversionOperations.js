057e1d71621f33f575079091ea03961ea8bfeb1b
itmeshruti <159559084+itmeshruti@users.noreply.github.com>
2025-03-25 16:44:48 +0000

    operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "33abc"
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Shruti" => true
+
+let number = 33
+let stringnum = String(number)
+console.log(stringnum)
+console.log(typeof stringnum)
