4c88d3fa47b57265e80256c5ff96eed1a3b132ad
itmeshruti <159559084+itmeshruti@users.noreply.github.com>
2025-03-27 13:35:34 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+//console.log(2>1);
+//console.log(2<1);
+//console.log(2>=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >=0); //true
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >=0);
+
+// === also checks the datatype
+
+console.log("2"===2);
