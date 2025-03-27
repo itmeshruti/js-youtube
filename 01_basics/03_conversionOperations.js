dc7152d9a0cf85603f806319119d104feefc6f6e
itmeshruti <159559084+itmeshruti@users.noreply.github.com>
2025-03-27 13:35:00 +0000

    conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof(score))
-console.log(typeof score)
+//console.log(typeof(score))
+//console.log(typeof score)
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber)
 
 let isLoggedIn = 1
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+//console.log(booleanisLoggedIn)
 
 // 1 => true; 0 => false
 // "" => false
@@ -21,5 +21,32 @@ console.log(booleanisLoggedIn)
 
 let number = 33
 let stringnum = String(number)
-console.log(stringnum)
-console.log(typeof stringnum)
+//console.log(stringnum)
+//console.log(typeof stringnum)
+
+let value = 3
+let negativevalue = -value
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2); power
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Shruti"
+let str3 = str1+str2 
+console.log(str3)
+
+//console.log("1"+2); //12
+//console.log(1+"2"); //12
+//console.log("1"+2+2); //122
+//console.log(1+2+"2"); //32
+
+console.log(+true); //1
+console.log(+"") //0
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);
